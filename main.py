a1e98052b5f9ead65747e1f91c2dd82c800e232a
Fjalar <fjalarhognason@gmail.com>
2026-06-10 14:01:34 +0200

    Three output neurons

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import numpy as np
 def main():
 
     inputs = [1.0, 2.0, 3.0, 2.5]
-    weights = [0.2, 0.8, -0.5, 1.0]
+    weights = [[0.2, 0.8, -0.5, 1.0],
+                [0.5, -0.91, 0.26, -0.5],
+                [-0.26, -0.27, 0.17, 0.87]]
 
-    bias = 2.0
+    biases = [2.0, 3.0, 0.5]
     
-    outputs = np.dot(weights, inputs) + bias
+    outputs = np.dot(weights, inputs) + biases
 
     print(outputs)
 
