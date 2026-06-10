381f7629551adc1e6ef7b06f2ea97d6e4c3003d4
Fjalar <fjalarhognason@gmail.com>
2026-06-10 13:56:32 +0200

    Page 23

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from nn!")
+import numpy as np
 
+def main():
+
+    inputs = [1.0, 2.0, 3.0, 2.5]
+    weights = [0.2, 0.8, -0.5, 1.0]
+
+    bias = 2.0
+    
+    outputs = np.dot(weights, inputs) + bias
+
+    print(outputs)
 
 if __name__ == "__main__":
     main()
