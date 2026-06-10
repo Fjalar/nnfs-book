ed6836b684989109b4c9f32c8a7c8ad3bc73d8b7
Fjalar <fjalarhognason@gmail.com>
2026-06-10 22:53:51 +0200

    Loss

--- main.py
+++ main.py
@@ -22,6 +22,37 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def forward(self, y_pred, y_true):
+        raise NotImplementedError
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7) # avoid division with zero
+
+        # handle either categorical labels (just index of correct label),
+        # or one-hot encoded labels (zeroes and a one on correct label)
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[ # index using correct labels as indices
+                range(samples),
+                y_true
+            ]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true,
+                axis = 1
+            )
+        else:
+            raise ValueError
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
 def main():
     np.random.seed(0) # for reproducibility
 
@@ -36,6 +67,8 @@ def main():
 
     activation2 = Activation_Softmax()
 
+    loss_function = Loss_CategoricalCrossentropy()
+
     dense1.forward(X)
 
     activation1.forward(dense1.output)
@@ -45,6 +78,10 @@ def main():
     activation2.forward(dense2.output)
 
     print(activation2.output[:5])
+
+    loss = loss_function.calculate(activation2.output, y)
+
+    print(f"loss: {loss}")
 
 if __name__ == "__main__":
     main()
