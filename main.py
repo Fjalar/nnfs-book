7c8c6c53b60ce163ebd25d81b990635003e0b86d
Fjalar <fjalarhognason@gmail.com>
2026-06-13 13:26:38 +0200

    Backward pass

--- main.py
+++ main.py
@@ -116,29 +116,36 @@ def main():
     # dataset
     X, y = spiral_data_generator(100, 3)
 
+    # network definition
     dense1 = Layer_Dense(2, 3)
-
     activation1 = Activation_ReLU()
-
     dense2 = Layer_Dense(3, 3)
-
-    dense1.forward(X)
-
-    activation1.forward(dense1.output)
-
-    dense2.forward(activation1.output)
-
     loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 
-    loss_activation.forward(dense2.output, y)
+    # forward pass
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    loss = loss_activation.forward(dense2.output, y)
 
+    # result after first forward pass
+    print(f"loss: {loss}")
     predictions = np.argmax(loss_activation.output, axis=1)
-
     if len(y.shape) == 2:
         y = np.argmax(y, axis=1)
     accuracy = np.mean(predictions == y)
-
     print(f"accuracy: {accuracy}")
+
+    # backward pass
+    loss_activation.backward(loss_activation.output, y)
+    dense2.backward(loss_activation.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
+
+    print(dense1.dweights)
+    print(dense1.dbiases)
+    print(dense2.dweights)
+    print(dense2.dbiases)
 
 if __name__ == "__main__":
     main()
