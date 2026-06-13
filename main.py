dad2cd69fce97eeff07be7aa3c3fabef1936fcca
Fjalar <fjalarhognason@gmail.com>
2026-06-13 21:02:56 +0200

    RMSprop optimizer

--- main.py
+++ main.py
@@ -178,6 +178,36 @@ class Optimizer_AdaGrad:
     def post_update_params(self):
         self.iterations += 1
 
+# Root mean square propagation
+class Optimizer_RMSprop:
+    def __init__(self, learning_rate=1.0, decay=0, epsilon=1e-7, rho=0.9):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.rho = rho
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1.0 / (1.0 + self.decay * self.iterations))
+
+    def update_params(self, layer):
+        if not hasattr(layer, "weight_cache"):
+            layer.weight_cache= np.zeros_like(layer.weights)
+            layer.bias_cache = np.zeros_like(layer.biases)
+
+        # update cache with squared current gradients
+        layer.weight_cache = self.rho * layer.weight_cache +  (1 - self.rho) * layer.dweights ** 2
+        layer.bias_cache = self.rho * layer.bias_cache + (1 - self.rho) * layer.dbiases ** 2
+
+        # apply updates
+        layer.weights -= self.current_learning_rate * layer.dweights / (np.sqrt(layer.bias_cache) + self.epsilon)
+        layer.biases -= self.current_learning_rate * layer.dbiases / (np.sqrt(layer.bias_cache) + self.epsilon)
+
+    def post_update_params(self):
+        self.iterations += 1
+
 def main():
     np.random.seed(0) # for reproducibility
 
@@ -189,8 +219,7 @@ def main():
     activation1 = Activation_ReLU()
     dense2 = Layer_Dense(64, 3)
     loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
-    optimizer = Optimizer_AdaGrad(decay=1e-4)
-
+    optimizer = Optimizer_RMSprop(learning_rate=0.02, decay=1e-5, rho=0.999)
 
     for epoch in range(10001):
         # forward pass
